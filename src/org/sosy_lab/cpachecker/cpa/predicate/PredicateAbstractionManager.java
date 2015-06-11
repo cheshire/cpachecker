cheshire/cpachecker

commit 6730702c79c82ea45bad54663bed0f4e8dbd1369

Change FormulaManagerView.extractAtoms to not uninstantiate the returned atoms, and remove parameter uninstantiate from all extract* methods. Not all callers need it, and the rest can simply call uninstantiate.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@16220 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/predicate/PredicateAbstractionManager.java
@@ -1075,7 +1075,7 @@ public class PredicateAbstractionManager {
       return ImmutableList.of(amgr.makeFalsePredicate());
     }
 
-    Collection<BooleanFormula> atoms = fmgr.extractAtoms(pFormula, splitItpAtoms);
+    Set<BooleanFormula> atoms = fmgr.uninstantiate(fmgr.extractAtoms(pFormula, splitItpAtoms));
 
     List<AbstractionPredicate> preds = new ArrayList<>(atoms.size());
 

src/org/sosy_lab/cpachecker/cpa/policyiteration/FormulaSlicingManager.java
@@ -1,6 +1,5 @@
 package org.sosy_lab.cpachecker.cpa.policyiteration;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -54,7 +53,7 @@ public class FormulaSlicingManager {
    */
   private Set<String> findClosure(BooleanFormula f, Predicate<String> seedCondition) {
     Set<String> closure = new HashSet<>();
-    Collection<BooleanFormula> atoms = fmgr.extractAtoms(f, false);
+    Set<BooleanFormula> atoms = fmgr.uninstantiate(fmgr.extractAtoms(f, false));
     boolean changed = true;
     while (changed) {
       changed = false;

src/org/sosy_lab/cpachecker/util/precondition/segkro/ExtractNewPreds.java
@@ -64,10 +64,6 @@ public class ExtractNewPreds {
     this.mgrv = pSolver.getFormulaManager();
   }
 
-  private Collection<BooleanFormula> extractLiterals(BooleanFormula pInputFormula) {
-    return mgrv.extractLiterals(pInputFormula, false); // TODO: check whether to use extractDisjuncts instead
-  }
-
   public List<BooleanFormula> extractNewPreds(Collection<BooleanFormula> pBasePredicates) throws SolverException, InterruptedException {
     final List<BooleanFormula> resultPredicates = Lists.newArrayList();
     final LinkedList<BooleanFormula> resultPredicatesPrime = Lists.newLinkedList();
@@ -171,7 +167,8 @@ public class ExtractNewPreds {
   public List<BooleanFormula> extractNewPreds(BooleanFormula pConjunctiveFormula) throws SolverException, InterruptedException {
     // Start with the list of basic predicates
     //  (extracted from the conjunctive formula)
-    Collection<BooleanFormula> literals = extractLiterals(pConjunctiveFormula);
+    // TODO: check whether to use extractDisjuncts instead
+    Collection<BooleanFormula> literals = mgrv.extractLiterals(pConjunctiveFormula);
     return extractNewPreds(literals);
   }
 

src/org/sosy_lab/cpachecker/util/precondition/segkro/MinCorePrio.java
@@ -138,8 +138,7 @@ public class MinCorePrio implements InterpolationWithCandidates {
     Preconditions.checkNotNull(pPhiMinus);
     Preconditions.checkNotNull(pPhiPlus);
 
-    Set<BooleanFormula> resultPredicates = Sets.newHashSet(
-        mgrv.extractLiterals(pPhiMinus, false));
+    Set<BooleanFormula> resultPredicates = Sets.newHashSet(mgrv.extractLiterals(pPhiMinus));
 
     if (!isInconsistent(pPhiMinus, pPhiPlus)) {
       logger.log(Level.WARNING, "MinCorePrio: Formulas not inconsistent! The over-approximation of the system might be insufficient!");

src/org/sosy_lab/cpachecker/util/precondition/segkro/Refine.java
@@ -25,6 +25,7 @@ package org.sosy_lab.cpachecker.util.precondition.segkro;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.sosy_lab.common.Pair;
 import org.sosy_lab.common.configuration.Configuration;
@@ -94,8 +95,8 @@ public class Refine implements PreconditionRefiner {
     helper = new PreconditionHelper(mgrv, pConfig, pLogger, pShutdownNotifier, pCfa);
   }
 
-  private Collection<BooleanFormula> literals(BooleanFormula pF, FormulaMode pMode) {
-    return mgrv.extractLiterals(pF, pMode == FormulaMode.UNINSTANTIATED);
+  private Set<BooleanFormula> uninstantiatedLiterals(BooleanFormula pF) {
+    return mgrv.uninstantiate(mgrv.extractLiterals(pF));
   }
 
   @VisibleForTesting
@@ -242,7 +243,7 @@ public class Refine implements PreconditionRefiner {
                 mgrv.instantiate(predsNew, instantiateWith),
                 t.getSuccessor()));
 
-        result.putAll(t.getSuccessor(), literals(preAtK.getFormula(), FormulaMode.UNINSTANTIATED));
+        result.putAll(t.getSuccessor(), uninstantiatedLiterals(preAtK.getFormula()));
       }
     }
 
@@ -294,8 +295,8 @@ public class Refine implements PreconditionRefiner {
     Builder<BooleanFormula> globalPreds = ImmutableList.builder();
     ImmutableMultimap.Builder<CFANode, BooleanFormula> localPreds = ImmutableMultimap.builder();
 
-    globalPreds.addAll(literals(pcViolation.getFormula(), FormulaMode.UNINSTANTIATED));
-    globalPreds.addAll(literals(pcValid.getFormula(), FormulaMode.UNINSTANTIATED));
+    globalPreds.addAll(uninstantiatedLiterals(pcViolation.getFormula()));
+    globalPreds.addAll(uninstantiatedLiterals(pcValid.getFormula()));
 
     // Get additional predicates from the states along the trace
     //    (or the WPs along the trace)...

src/org/sosy_lab/cpachecker/util/precondition/segkro/RefineSolverBasedItp.java
@@ -25,6 +25,7 @@ package org.sosy_lab.cpachecker.util.precondition.segkro;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 
 import org.sosy_lab.common.Pair;
 import org.sosy_lab.common.configuration.Configuration;
@@ -94,8 +95,8 @@ public class RefineSolverBasedItp implements PreconditionRefiner {
     helper = new PreconditionHelper(mgrv, pConfig, pLogger, pShutdownNotifier, pCfa);
   }
 
-  private Collection<BooleanFormula> literals(BooleanFormula pF, FormulaMode pMode) {
-    return mgrv.extractLiterals(pF, pMode == FormulaMode.UNINSTANTIATED);
+  private Set<BooleanFormula> uninstantiatedLiterals(BooleanFormula pF) {
+    return mgrv.uninstantiate(mgrv.extractLiterals(pF));
   }
 
   @VisibleForTesting
@@ -242,7 +243,7 @@ public class RefineSolverBasedItp implements PreconditionRefiner {
                 mgrv.instantiate(predsNew, instantiateWith),
                 t.getSuccessor()));
 
-        result.putAll(t.getSuccessor(), literals(preAtK.getFormula(), FormulaMode.UNINSTANTIATED));
+        result.putAll(t.getSuccessor(), uninstantiatedLiterals(preAtK.getFormula()));
       }
     }
 
@@ -294,8 +295,8 @@ public class RefineSolverBasedItp implements PreconditionRefiner {
     Builder<BooleanFormula> globalPreds = ImmutableList.builder();
     ImmutableMultimap.Builder<CFANode, BooleanFormula> localPreds = ImmutableMultimap.builder();
 
-    globalPreds.addAll(literals(pcViolation.getFormula(), FormulaMode.UNINSTANTIATED));
-    globalPreds.addAll(literals(pcValid.getFormula(), FormulaMode.UNINSTANTIATED));
+    globalPreds.addAll(uninstantiatedLiterals(pcViolation.getFormula()));
+    globalPreds.addAll(uninstantiatedLiterals(pcValid.getFormula()));
 
     // Get additional predicates from the states along the trace
     //    (or the WPs along the trace)...

src/org/sosy_lab/cpachecker/util/precondition/segkro/rules/PatternBasedRule.java
@@ -98,7 +98,7 @@ public abstract class PatternBasedRule extends AbstractRule {
 
   @Override
   public Set<BooleanFormula> apply(BooleanFormula pInput) throws SolverException, InterruptedException {
-    return apply(fmv.extractLiterals(pInput, false), HashMultimap.<String, Formula>create());
+    return apply(fmv.extractLiterals(pInput), HashMultimap.<String, Formula>create());
   }
 
   protected Formula substituteInParent(

src/org/sosy_lab/cpachecker/util/predicates/interfaces/view/FormulaManagerView.java
@@ -25,6 +25,7 @@ package org.sosy_lab.cpachecker.util.predicates.interfaces.view;
 
 
 import static com.google.common.base.Preconditions.*;
+import static com.google.common.collect.FluentIterable.from;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -929,6 +930,17 @@ public class FormulaManagerView {
         );
   }
 
+  public Set<BooleanFormula> uninstantiate(Collection<BooleanFormula> formulas) {
+    return from(formulas)
+        .transform(new Function<BooleanFormula, BooleanFormula>() {
+          @Override
+          public BooleanFormula apply(BooleanFormula pInput) {
+            return uninstantiate(pInput);
+          }
+        })
+        .toSet();
+  }
+
   /**
    * Apply an arbitrary renaming to all free variables and UFs in a formula.
    * @param pFormula The formula in which the renaming should occur.
@@ -1054,7 +1066,6 @@ public class FormulaManagerView {
 
   /**
    * Extract all atoms of a given boolean formula.
-   * All atoms get un-instantiated!!
    */
   public Collection<BooleanFormula> extractAtoms(BooleanFormula f, boolean splitArithEqualities) {
     return myExtractAtoms(f, splitArithEqualities,
@@ -1063,7 +1074,7 @@ public class FormulaManagerView {
           public boolean apply(BooleanFormula pInput) {
             return unsafeManager.isAtom(pInput);
           }
-        }, true);
+        });
   }
 
   /**
@@ -1078,12 +1089,10 @@ public class FormulaManagerView {
             // treat as atomic if formula is neither "not" nor "and"
             return !(booleanFormulaManager.isNot(pInput) || booleanFormulaManager.isAnd(pInput));
           }
-        }, false);
+        });
   }
 
-  public Collection<BooleanFormula> extractLiterals(
-      BooleanFormula f,
-      boolean uninstanciate) {
+  public Collection<BooleanFormula> extractLiterals(BooleanFormula f) {
 
     return myExtractAtoms(f, false /*splitArithEqualities not supported for literals */,
         new Predicate<BooleanFormula>() {
@@ -1095,11 +1104,11 @@ public class FormulaManagerView {
             return unsafeManager.isAtom(pInput)
                 || booleanFormulaManager.isNot(pInput);
           }
-        }, uninstanciate);
+        });
   }
 
   private Collection<BooleanFormula> myExtractAtoms(BooleanFormula f, boolean splitArithEqualities,
-      Predicate<BooleanFormula> isLowestLevel, boolean uninstanciate) {
+      Predicate<BooleanFormula> isLowestLevel) {
     Set<BooleanFormula> handled = new HashSet<>();
     List<BooleanFormula> atoms = new ArrayList<>();
 
@@ -1116,10 +1125,6 @@ public class FormulaManagerView {
       }
 
       if (isLowestLevel.apply(tt)) {
-        if (uninstanciate) {
-          tt = uninstantiate(tt);
-        }
-
         if (splitArithEqualities
             && myIsPurelyArithmetic(tt)) {
           BooleanFormula split = unsafeManager.splitNumeralEqualityIfPossible(tt);

commit 1d1f1638478ef4626dc5b517be3373b1b0f37e20

Fix bug introduced in r16220: predicates of PredicateCPA had SSA indices In this revision it was forgotten to introduce a call to uninstantiate in one of the callers of extractAtoms(), which previously did the uninstantiate itself. This meant that all the predicates used by the PredicateCPA still had variables with SSA indices although they are meant to have no indices. This works in fact, but leads to a much higher number of predicates, because the same predicate can now appear with different SSA indices. Thus we revert this back to the previous behavior.
git-svn-id: https://svn.sosy-lab.org/software/cpachecker/trunk@17001 4712c6d2-40bb-43ae-aa4b-fec3f1bdfe4c

src/org/sosy_lab/cpachecker/cpa/predicate/PredicateAbstractionManager.java
@@ -540,7 +540,7 @@ public class PredicateAbstractionManager {
       amgr.makePredicate(atom);
       extractPredicates(atom);
     }
-    
+
     return amgr.buildRegionFromFormula(eliminationResult);
 
   }
@@ -1067,7 +1067,7 @@ public class PredicateAbstractionManager {
     List<AbstractionPredicate> preds = new ArrayList<>(atoms.size());
 
     for (BooleanFormula atom : atoms) {
-      preds.add(amgr.makePredicate(atom));
+      preds.add(amgr.makePredicate(fmgr.uninstantiate(atom)));
     }
 
     amgr.reorderPredicates();